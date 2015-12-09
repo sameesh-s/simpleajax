9603d96a1fd0e10623e3c19288719499b5327acd
sameesh-s <sameesh.fisat@gmail.com>
2015-12-10 00:09:15 +0530

    news uploading

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+echo "<input type='text' id='txtName' name='name' value='";
+echo time();
+echo "'>";
+echo "<img src='images/file2.jpg' alt='imge loading failed' height='42' width='42'>";
+
+?>
