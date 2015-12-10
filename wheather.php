c5fff5a1391f8509f65252ce6fa52e32742793df
sameesh-s <sameesh.fisat@gmail.com>
2015-12-10 18:32:32 +0530

    categorized news flashing

---
+++ wheather.php
@@ -0,0 +1,17 @@
+<?php
+/*if(!isset($_SEESSION))
+	{
+	}
+else
+	{
+	}
+
+*/
+$con = mysqli_connect("localhost","root","mca");
+mysqli_select_db($con,"mcadb") or die(mysqli_error($con));
+$rslt=mysqli_query($con,"select * from news_tab where news_id < 8 AND category = 4");
+
+$row=mysqli_fetch_row($rslt);
+
+echo $row[1]."<br><img src='images/".$row[4]."' alt='imge loading failed' height='200' width='200'><br>|Wheather<hr>";
+?>
